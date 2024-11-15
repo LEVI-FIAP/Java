24cd5239c8c550866d3bc21508ee00f92c548f94
fFukurou <supevi1@gmail.com>
2024-11-15 16:01:09 -0300

    idk anymore

---
+++ src/main/java/br/com/gslevi/Main.java
@@ -0,0 +1,12 @@
+package br.com.gslevi;
+
+import br.com.gslevi.dao.RelatorioDAO;
+import br.com.gslevi.model.Relatorio;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hello World!");
+    }
+}
